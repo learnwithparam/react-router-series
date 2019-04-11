3db2531748a6f314f108c4b1024118c2d89e41a3
Param-Harrison <paramanantham@live.com>
2019-04-11 08:29:51 +0300

    feat: basic routing with example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+
+const IndexPage = () => {
+  return (
+    <h3>Home Page</h3>
+  );
+};
+
+const AboutPage = () => {
+  return (
+    <h3>About Page</h3>
+  );
+};
 
 const App = () => {
   return (
     <section className="App">
-      <h1>React Router example</h1>
+      <Router>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Route exact path="/" component={IndexPage} />
+        <Route exact path="/about" component={AboutPage} />
+      </Router>
+      <a href="/about">about with browser reload</a>
     </section>
   );
 };
