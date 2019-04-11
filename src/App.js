02bfb1cd295e2dac350ca82ecc64919846f689bb
Param-Harrison <paramanantham@live.com>
2019-04-11 08:26:28 +0300

    fix: basic state

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
 import React from 'react';
-import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-
-const IndexPage = () => {
-  return (
-    <h3>Home Page</h3>
-  );
-};
-
-const AboutPage = () => {
-  return (
-    <h3>About Page</h3>
-  );
-};
 
 const App = () => {
   return (
     <section className="App">
-      <Router>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Route exact path="/" component={IndexPage} />
-        <Route exact path="/about" component={AboutPage} />
-      </Router>
-      <a href="/about">about with browser reload</a>
+      <h1>React Router example</h1>
     </section>
   );
 };
